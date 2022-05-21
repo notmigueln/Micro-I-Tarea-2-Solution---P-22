2274d453f67199f30d462d7a4a5bf3ff255461fd
Arturo Aguilar <arturo.aguilar@itam.mx>
2022-05-20 21:47:07 -0500

    Correcciones de la Tarea

--- Sol_Tarea_2.R
+++ Sol_Tarea_2.R
@@ -4,13 +4,10 @@
 
 # Para ejecutar este script desde su computadora pueden guardar
 # las bases de datos que emplea en un folder en su computadora
-# y establecer dicho folder como el working directory cambiando
-# la siguiente linea [ojo: utilicen las diagonales igual que 
-# abajo sin importar si usan PC o Mac]. Alternativamente pueden
-# crear un proyecto referenciado a dicho folder como vimos en 
-# clase
+# y en dicho folder crear un proyecto referenciado a ese folder
+# como vimos en clase
 
-#Cargamos los paquetes que utilizaremos en la tarea
+# Cargamos los paquetes que utilizaremos en la tarea
 
 library(sandwich)
 library(cobalt) 
@@ -22,11 +19,12 @@ library(car)
 library(jtools) 
 library(dplyr)
 library(multcomp)
+library(matlib)
 
 #Cargamos las base de datos frmgham
 
 frmgham<-read.csv("frmgham.csv")
-frmgham <- data.frame(frmgham)
+frmgham <- as.data.frame(frmgham)
 
 # ====// Pregunta 1 \\====
 
@@ -57,19 +55,19 @@ frmgham <- frmgham %>%
 mco <- lm(CVD ~ menor_50, 
             data = frmgham)
 es_mco<- sqrt(diag(vcovHC(mco, type = "HC1")))
-stargazer(mco,type = "text",se=es_mco)
+stargazer(mco,type = "text",se=list(es_mco))
 
 # ====// Inciso (c)
 
 # Apartado (III)
 
-#Calculamos todos los datos que necesitamos para estimar el esstadítico t
+#Calculamos todos los datos que necesitamos para estimar el estadítico t
 (X_menor <- mean(frmgham[frmgham$menor_50 == 1, 'CVD']))
 (X_mayor <- mean(frmgham[frmgham$menor_50 == 0, 'CVD']))
 (s_menor <- var(frmgham[frmgham$menor_50 == 1, 'CVD']))
 (s_mayor <- var(frmgham[frmgham$menor_50 == 0, 'CVD']))
-(n_menor <- length(frmgham[frmgham$menor_50 == 1, 'CVD']))
-(n_mayor <- length(frmgham[frmgham$menor_50 == 0, 'CVD']))
+(n_menor <- sum(!is.na(frmgham[frmgham$menor_50 == 1, 'CVD'])))
+(n_mayor <- sum(!is.na(frmgham[frmgham$menor_50 == 0, 'CVD'])))
 
 #Estimamos t
 (t_num <- (X_menor-X_mayor))
@@ -77,23 +75,34 @@ stargazer(mco,type = "text",se=es_mco)
 (t<-(t_num/t_den))
 
 #Varianza heterocedástica MCO
-(var_het_mco<-(s_menor/n_menor)*((n_menor-1)/n_menor)+(s_mayor/n_mayor)*((n_mayor-1)/n_mayor))
+(var_het_mco<-(s_menor/n_menor)*(n_menor/(n_menor-1))+(s_mayor/n_mayor)*(n_mayor/(n_mayor-1)))
 (se_het_mco <- sqrt(var_het_mco))
 
 # ====// Pregunta 2 \\====
 
-#Creamos las variables que necesitaremos
+#Creamos las variables que necesitaremos, nos quedamos con las
+# variables necesarias para las estimaciones y quitamos observaciones
+# con missing values. 
+# Es una practica importante que su numero de observaciones sea
+# consistente entre columnas y evitar borrar observaciones de mas
+# involucrando variables que no empleamos en el analisis.
 frmgham <- frmgham %>% mutate(AGE_2= AGE^2,
-                              SMKR_CIG = SMKR*CIGDAY)
+                              SMKR_CIG = SMKR*CIGDAY,
+                              LOGCHOL = log(TOTCHOL),
+                              LOGBMI = log(BMI)) %>%
+          dplyr::select(CVD,AGE,TOTCHOL,HDLC,SYSBP,BPMEDS,SMKR,SMKR_CIG,DIABETES,
+                 LOGCHOL,LOGBMI,WOMEN,AGE_2,PREVCHD,BMI) %>%
+          na.omit()
+  
 
 #Especificación (1)
 mco_1 <- lm(CVD ~ AGE + log(TOTCHOL) + HDLC + SYSBP + BPMEDS + SMKR + SMKR_CIG + DIABETES, 
-            data = subset(frmgham, WOMEN == 1))
+            data = frmgham %>% filter(WOMEN == 1))
 es_mco_1<- sqrt(diag(vcovHC(mco_1, type = "HC1")))
 
 #Especificación (2)
 mco_2 <- lm(CVD ~ AGE + log(TOTCHOL) + HDLC + SYSBP + BPMEDS + SMKR + SMKR_CIG + DIABETES, 
-            data = subset(frmgham, WOMEN == 0))
+            data = frmgham %>% filter(WOMEN == 0))
 es_mco_2<- sqrt(diag(vcovHC(mco_2, type = "HC1")))
 
 #Especificación (3)
@@ -163,22 +172,19 @@ Gamma <- mco_5$coefficients[4]
 #Desviación estándar de SYSBP
 (sd_sysbp <- sd(frmgham$SYSBP, na.rm = T))
 
-#Datos promedio 
-(CVD_promedio <- mean(frmgham$CVD, na.rm = T))
-(SYSBP_promedio <- mean(frmgham$SYSBP, na.rm = T))
-
 #Cambio de CVD asociado a la columna 3
 (Ch_CVD <- sd_sysbp*(mco_3$coef["SYSBP"]))
+
+# Como % de la media
+CVD_promedio <- mean(frmgham$CVD, na.rm = T)
 (Ch_percent_CVD <- 100*Ch_CVD/CVD_promedio)
 
 #Error Estándar
-l <- c(0,0,0,0,0,sd_sysbp,0,0,0,0) 
-(mvcov_mco_3 <- vcovHC(mco_3, type = "HC1"))
-(se_5a <- sqrt(t(l) %*% mvcov_mco_3 %*% l))
+(se_5a <- sd_sysbp*es_mco_3["SYSBP"])
 
 #Intervalo de confianza
-(IC_5a <- (100/CVD_promedio)*c(t(l) %*% mco_3$coef - 1.65*se_5a,
-                               t(l) %*% mco_3$coef + 1.65*se_5a))  
+(IC_5a <- c(Ch_CVD - 1.64*se_5a,Ch_CVD + 1.64*se_5a))  
+(IC_m5a <- 100*IC_5a*(1/CVD_promedio))  
 
 
 # ====// Pregunta 6 \\====
@@ -186,13 +192,12 @@ l <- c(0,0,0,0,0,sd_sysbp,0,0,0,0)
 # ====// Inciso (a)
 
 #Cargamos los datos médicos de Oswald
-Os_data_1_2 <- c(1,43,log(240),35,160,0,1,3,0)
+Os_data_2 <- c(1,43,log(240),35,160,0,1,3,0)
 Os_data_3 <-c(1,43,0,log(240),35,160,0,1,3,0)
-Os_data_4 <-c(1,43,43**2,0,35,160,0,0,1,0,log(36))
+Os_data_4 <-c(1,43,43^2,0,35,160,0,0,1,0,log(36))
 
 #Evaluamos las especificaciones
-(eval_1<-t(Os_data_1_2)%*%mco_1$coefficients)
-(eval_2<-t(Os_data_1_2)%*%mco_2$coefficients)
+(eval_2<-t(Os_data_2)%*%mco_2$coefficients)
 (eval_3<-t(Os_data_3)%*%mco_3$coefficients)
 (eval_4<-t(Os_data_4)%*%mco_4$coefficients)
 
@@ -200,7 +205,7 @@ Os_data_4 <-c(1,43,43**2,0,35,160,0,0,1,0,log(36))
 
 #Construimos y evaluamos los datos clínicos de Oswald
 Os_today <- Os_data_4
-Os_tomorrow <- c(1,48,48**2,0,40,130,0,1,1,0,log(28))
+Os_tomorrow <- c(1,48,48^2,0,40,130,0,1,1,0,log(28))
 (eval_today<-t(Os_today)%*%mco_4$coefficients)
 (eval_tomorrow<-t(Os_tomorrow)%*%mco_4$coefficients)
 
@@ -208,25 +213,31 @@ Os_tomorrow <- c(1,48,48**2,0,40,130,0,1,1,0,log(28))
 diff<-Os_tomorrow-Os_today
 
 #Predicción diferencia
-(diff_p<-t(diff)%*%mco_4$coefficients)
+num_6b <- t(diff)%*%mco_4$coefficients
+den_6b <- sqrt(t(diff)%*%vcovHC(mco_4, type = "HC1")%*%diff)
 
-#Evaluación de la hipótesis
-hyp_1 <-linearHypothesis(mco_4,diff,white.adjust = 'hc1', rhs=-0.02)
+t_6b <- num_6b/den_6b
+(pval_6b<-2*(1-pnorm(abs(t_6b),0,1,lower.tail = T)))
+
+#Evaluación alternativa de la hipótesis
+(hyp_1 <-linearHypothesis(mco_4,diff,white.adjust = 'hc1', rhs=-0.02))
 (t_1<-sqrt(hyp_1$F[2]))
 (pval_1<-2*(pnorm(t_1,0,1,lower.tail = F)))
 
 # ====// Inciso (c)
 
-# Utilizaremos la función glht para calcular el intervalo de confianza
+# Podemos usar la misma formula que 5a
+(IC_6b <- c(num_6b - 1.96*den_6b,num_6b + 1.96*den_6b))  
+
+# Alternativamente podemos utilizar la función glht 
 
 #Para poder utilizar la función, renombramos la variable log(BMI) de la estimación 4 a log_BMI
 log_BMI <- log(frmgham$BMI)
 mco_4_bis <- lm(CVD ~ AGE + AGE_2 + WOMEN + HDLC + SYSBP + PREVCHD + BPMEDS + SMKR + DIABETES + log_BMI, 
             data = frmgham)
-es_mco_4_bis<- sqrt(diag(vcovHC(mco_4_bis, type = "HC1")))
 
 # Guardamos la combinacion lineal como un objeto llamado "lincom".
-lincom <- glht(mco_4_bis, linfct = c("5*AGE + 455*AGE_2 + 5*HDLC - 30*SYSBP + BPMEDS - 0.25*log_BMI = 0.02"), vcov=vcovHC(mco_4, type="HC1"))
+lincom <- glht(mco_4_bis, linfct = c("5*AGE + 455*AGE_2 + 5*HDLC - 30*SYSBP + BPMEDS - 0.25*log_BMI = -0.02"), vcov=vcovHC(mco_4, type="HC1"))
 
 # Obtenemos el intervalo de confianza.
 confint(lincom)
@@ -234,34 +245,39 @@ confint(lincom)
 # ====// Pregunta 7 \\====
 
 # ====// Inciso (a)
+# Plantear la matriz L que necesitamos
+mco_3 <- lm(CVD ~ AGE + WOMEN + log(TOTCHOL) + HDLC + SYSBP + BPMEDS + SMKR + SMKR_CIG + DIABETES, 
+            data = frmgham)
+
+mat_7 <- c(0,0,0,0,0,0,0,1,0,0,
+           0,0,0,0,0,0,0,0,1,0)
+L_7 <- matrix(mat_7,nrow=2,ncol = 10, byrow = T)
+
+F1 <- L_7%*%mco_3$coefficients
+F2 <- solve(L_7%*% vcovHC(mco_3, type = "HC1") %*%t(L_7))
+(F_7 <- t(F1)%*%F2%*%F1)
+(pval_7 <- pchisq(F_7,2,lower.tail=F))
 
 linearHypothesis(mco_3,c("SMKR=0","SMKR_CIG=0"),white.adjust="hc1")
 
 # ====// Inciso (b)
-
-#Obtenemos la muestra de la especificación (3)
-frmgham_bis <- frmgham %>% 
-      drop_na(RANDID,CVD, AGE, WOMEN, TOTCHOL, HDLC, SYSBP, BPMEDS, SMKR, SMKR_CIG, DIABETES)
 
 #Creamos el vector donde guardaremos los resultados de Bootstrap
 betas_diff <-c()
 
 for (i in 1:1000) {
   
-  muestra_sim <- sample_n(frmgham_bis, size = nrow(frmgham_bis), replace = T)
+  muestra_sim <- sample_n(frmgham, size = nrow(frmgham), replace = T)
   
   #Especificación mujeres
   mc_1 <- lm(CVD ~ AGE + log(TOTCHOL) + HDLC + SYSBP + BPMEDS + SMKR + SMKR_CIG + DIABETES, 
-              data = subset(muestra_sim, WOMEN == 1))
-  es_mc_1<- sqrt(diag(vcovHC(mc_1, type = "HC1")))
-  
+              data = muestra_sim %>% filter(WOMEN == 1))
   #Especificación hombres
   mc_2 <- lm(CVD ~ AGE + log(TOTCHOL) + HDLC + SYSBP + BPMEDS + SMKR + SMKR_CIG + DIABETES, 
-              data = subset(muestra_sim, WOMEN == 0))
-  es_mc_2<- sqrt(diag(vcovHC(mc_2, type = "HC1")))
-  
+             data = muestra_sim %>% filter(WOMEN == 0))
+
   #Calculamos la diferencia y la guardamos
-  dif_sim <- mc_1$coefficients[2]-mc_2$coefficients[2]
+  dif_sim <- mc_1$coefficients["AGE"]-mc_2$coefficients["AGE"]
   betas_diff <-c(betas_diff,dif_sim)
   
 }
